cbd6ac351383eac2f106d43a3caa3558ed9aabaf
Robotic-ui <84217158+Robotic-ui@users.noreply.github.com>
2021-06-30 01:02:30 -0400

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -4,18 +4,18 @@ import random
 
 delay = 0.1
 
-#scores
+# scores
 score = 0
 high_score = 0
 
-#set up screen
+# setup screen
 window = turtle.Screen()
 window.title("Snake Game")
 window.bgcolor('black')
 window.setup(width=600, height=600)
 window.tracer(0)
 
-#snake head
+# snake head
 head = turtle.Turtle()
 head.speed(0)
 head.shape("square")
@@ -24,7 +24,7 @@ head.penup()
 head.goto(0,0)
 head.direction = "stop"
 
-#snake food
+# food
 food= turtle.Turtle()
 food.speed(0)
 food.shape("square")
@@ -34,7 +34,7 @@ food.goto(0,100)
 
 segments = []
 
-#scoreboards (sc)
+# scoreboards (sc)
 sc = turtle.Turtle()
 sc.speed(0)
 sc.shape("square")
@@ -44,7 +44,7 @@ sc.hideturtle()
 sc.goto(0,260)
 sc.write("score: 0  High score: 0", align = "center", font=("ds-digital", 24, "normal"))
 
-#Functions
+# functions
 def go_up():
     if head.direction != "down":
         head.direction = "up"
@@ -71,14 +71,14 @@ def move():
         x = head.xcor()
         head.setx(x+20)
 
-#keyboard bindings
+# keyboard bindings
 window.listen()
 window.onkeypress(go_up, "w")
 window.onkeypress(go_down, "s")
 window.onkeypress(go_left, "a")
 window.onkeypress(go_right, "d")
 
-#MainLoop
+# mainloop
 while True:
     window.update()
 
@@ -88,29 +88,29 @@ while True:
         head.goto(0,0)
         head.direction = "stop"
 
-        #hide the segments of body
+        # hide the segments of body
         for segment in segments:
             segment.goto(1000,1000) #out of range
-        #clear the segments
+        # clear the segments
         segments.clear()
 
-        #reset score
+        # reset score
         score = 0
 
-        #reset delay
+        # reset delay
         delay = 0.1
 
         sc.clear()
         sc.write("score: {}  High score: {}".format(score, high_score), align="center", font=("ds-digital", 24, "normal"))
 
-    #check collision with food
+    # check collision with food
     if head.distance(food) <20:
         # move the food to random place
         x = random.randint(-290,290)
         y = random.randint(-290,290)
         food.goto(x,y)
 
-        #add a new segment to the head
+        # add a new segment to the head
         new_segment = turtle.Turtle()
         new_segment.speed(0)
         new_segment.shape("square")
@@ -118,10 +118,10 @@ while True:
         new_segment.penup()
         segments.append(new_segment)
 
-        #shorten the delay
+        # shorten the delay
         delay -= 0.001
         
-        #increase the score
+        # increase the score
         score += 10
 
         if score > high_score:
@@ -129,12 +129,12 @@ while True:
         sc.clear()
         sc.write("score: {}  High score: {}".format(score,high_score), align="center", font=("ds-digital", 24, "normal")) 
 
-    #move the segments in reverse order
+    # move the segments in reverse order
     for index in range(len(segments)-1,0,-1):
         x = segments[index-1].xcor()
         y = segments[index-1].ycor()
         segments[index].goto(x,y)
-    #move segment 0 to head
+    # move segment 0 to head
     if len(segments)>0:
         x = head.xcor()
         y = head.ycor()
@@ -142,21 +142,21 @@ while True:
 
     move()
 
-    #check for collision with body
+    # check for collision with body
     for segment in segments:
         if segment.distance(head)<20:
             time.sleep(1)
             head.goto(0,0)
             head.direction = "stop"
 
-            #hide segments
+            # hide segments
             for segment in segments:
                 segment.goto(1000,1000)
             segments.clear()
             score = 0
             delay = 0.1
 
-            #update the score     
+            # update the score     
             sc.clear()
             sc.write("score: {}  High score: {}".format(score,high_score), align="center", font=("ds-digital", 24, "normal"))
     time.sleep(delay)
